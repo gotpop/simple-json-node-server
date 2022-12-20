58a4f12754e9fd734630137db06130e2350ab7fd
GotPop <liam@gotpop.net>
2022-12-20 15:41:12 +0000

    Add express

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.get("/", (request, response) => {
     response.send("Hi there");
 });
 
-app.listen(9000, () => {
-    console.log("Listen on the port 9000...");
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
 });
