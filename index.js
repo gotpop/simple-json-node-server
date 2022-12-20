f69f13a613e36b738e6760282f01dee35c17370b
GotPop <liam@gotpop.net>
2022-12-20 17:18:10 +0000

    Add express

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const express = require("express");
+import express from 'express'
+import { shopItems } from './data/shop.js'
+
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 9000;
 
 app.get("/", (request, response) => {
-    response.send("Hi there");
+    response.json(shopItems);
 });
 
 app.listen(port, () => {
